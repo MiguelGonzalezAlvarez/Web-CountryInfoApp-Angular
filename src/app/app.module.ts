a16dafd9edbe36258c9098780705c7945b95e158
MiguelMuros <miggonzalv@gmail.com>
2021-09-28 19:39:25 +0200

    feat: add table and search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
