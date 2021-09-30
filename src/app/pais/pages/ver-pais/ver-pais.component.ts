800e9dd444fa4eeb66988efe504c1dcd83cd8cfa
MiguelMuros <miggonzalv@gmail.com>
2021-09-30 23:03:02 +0200

    feat: finish country detail screen structure but without good styling yet

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
+import { Pais } from '../../interfaces/pais.interface';
+
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,19 +13,19 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor
-    (
-      private activatedRoute: ActivatedRoute,
-      private paisService: PaisService
-    ) { }
+  pais!: Pais;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) { }
 
   ngOnInit(): void {
     // Utilizamos la desestructuracion de objetos para obetener el id de los params
-    this.activatedRoute.params.subscribe(({ id }) => {
-      this.paisService.getPaisPorAlpha(id).subscribe(response => {
-
-      });
-    });
+    // Usamos el switchMap para evitar tener que usar dos subscribe ya que params es tambien un observable
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)), tap(console.log))
+      .subscribe(pais => this.pais = pais);
   }
 
 }
