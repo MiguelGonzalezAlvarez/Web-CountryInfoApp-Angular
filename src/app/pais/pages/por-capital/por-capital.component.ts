55f87dc1759a6b2c7d20eebd24677995ffccf527
MiguelMuros <miggonzalv@gmail.com>
2021-10-04 20:43:15 +0200

    feat: adding empty terms validation

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -5,34 +5,64 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styles: [
-  ]
+  styles: [`
+      li { 
+        cursor: pointer; 
+      }
+  `]
 })
 export class PorCapitalComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  mostrarSugerencias = false;
+  terminoAlert = false;
+
   paises: Pais[] = [];
+  paisesSugeridos: Pais[] = [];
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string): void {
-    this.hayError = false;
-    this.termino = termino;
+    // Si nos pasan un termino vacio no se realiza la busqueda de paises
+    if (!termino.length) {
+      this.mostrarSugerencias = false;
+      this.terminoAlert = true;
+    }
+    else {
+      this.terminoAlert = false;
+      this.mostrarSugerencias = false;
+      this.hayError = false;
+      this.termino = termino;
 
-    this.paisService.buscarCapital(termino).subscribe((response) => {
-      // La respuesta que nos den rellena la tabla que mostramos
-      this.paises = response;
-    }, () => {
-      // Si se produce un error se pone vacia la tabla de paises
-      this.hayError = true;
-      this.paises = [];
-    });
+      this.paisService.buscarCapital(termino).subscribe((response) => {
+        // La respuesta que nos den rellena la tabla que mostramos
+        this.paises = response;
+      }, () => {
+        // Si se produce un error se pone vacia la tabla de paises
+        this.hayError = true;
+        this.paises = [];
+      });
+    }
   }
 
-  sugerencias(event: any) {
-    this.hayError = false;
-    // TODO: crear sugerencias
-
+  sugerencias(termino: string) {
+    // Si nos pasan un termino vacio no se realiza la busqueda de sugerencias
+    // No es necesario porner la alerta de termino vacio porque en el autocomplete no salen opciones
+    if (!termino.length) {
+      this.mostrarSugerencias = false;
+    }
+    else {
+      this.terminoAlert = false;
+      this.hayError = false;
+      this.termino = termino;
+      this.mostrarSugerencias = true;
+      
+      // Mostramos solamente tres paises como sugerencias
+      this.paisService.buscarCapital(termino).subscribe(
+        (response) => this.paisesSugeridos = response.splice(0, 3),
+        () => this.paisesSugeridos = []);
+    }
   }
+
 }
