fd162cd928e126f6e379a7030397ec28aae3a198
MiguelMuros <miggonzalv@gmail.com>
2021-10-04 19:14:48 +0200

    refactor: change continet type of search

--- src/app/pais/pages/por-continente/por-continente.component.ts
+++ src/app/pais/pages/por-continente/por-continente.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Pais } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-continente',
   templateUrl: './por-continente.component.html',
-  styles: [
+  styles: [`
+    button {
+        margin-right: 5px;
+    }
+  `
   ]
 })
 export class PorContinenteComponent {
+
+  continentes: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  continenteActivo: string = 'Africa';
 
   termino: string = '';
   hayError: boolean = false;
@@ -30,10 +37,21 @@ export class PorContinenteComponent {
     });
   }
 
+  activarContinente(continente: string): void {
+    this.continenteActivo = continente;
+    if (continente !== this.continenteActivo) {
+      // Si el continente no cambia no es necesario llamar de nuevo a la api
+      this.buscar(continente);
+    }
+  }
+
+  getClaseCss(continente: string): string {
+    return (continente === this.continenteActivo) ? 'btn btn-primary' : 'btn sbtn-outline-primary'
+  }
+
   sugerencias(event: any) {
     this.hayError = false;
     // TODO: crear sugerencias
-
   }
 
 }
